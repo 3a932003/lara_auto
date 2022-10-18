c0afe92633290d49950c801e5bc363ff9fdbf4c0
xu qian su <s3a932003@student.ncut.edu.tw>
2022-10-18 11:54:12 +0800

    時間

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@
 	$hello = new Demo\HelloWorld();
 	use Demo\HelloWorld as World;//類別另取別名
 	$world = new World();
+echo "<br>";
     use Monolog\Level;
     use Monolog\Logger;
     use Monolog\Handler\StreamHandler;
@@ -22,4 +23,52 @@
     // add records to the log
     $log->warning('Foo');
     $log->error('Bar');
+use Carbon\Carbon;
+echo "本地現在時間";
+printf("Right now is %s", Carbon::now()->toDateTimeString());
+echo "<br>";
+echo "vancouver現在時間";
+printf("Right now in Vancouver is %s", Carbon::now('America/Vancouver'));  //implicit __toString()
+echo "<br>";
+$tomorrow = Carbon::now()->addDay();
+$lastWeek = Carbon::now()->subWeek();
+$nextSummerOlympics = Carbon::createFromDate(2016)->addYears(4);
+
+$officialDate = Carbon::now()->toRfc2822String();
+
+$howOldAmI = Carbon::createFromDate(1975, 5, 21)->age;
+
+$noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Europe/London');
+
+$internetWillBlowUpOn = Carbon::create(2038, 01, 19, 3, 14, 7, 'GMT');
+
+// Don't really want this to happen so mock now
+Carbon::setTestNow(Carbon::createFromDate(2000, 1, 1));
+
+// comparisons are always done in UTC
+if (Carbon::now()->gte($internetWillBlowUpOn)) {
+    die();
+}
+
+// Phew! Return to normal behaviour
+Carbon::setTestNow();
+
+if (Carbon::now()->isWeekend()) {
+    echo 'Party!';
+}
+// Over 200 languages (and over 500 regional variants) supported:
+echo Carbon::now()->subMinutes(2)->diffForHumans();// '2 minutes ago'
+echo "<br>";
+echo Carbon::now()->subMinutes(2)->locale('zh_CN')->diffForHumans(); // '2分钟前'
+echo "<br>";
+echo "英文";
+echo Carbon::parse('2019-07-23 14:51')->isoFormat('LLLL'); // 'Tuesday, July 23, 2019 2:51 PM'
+echo "<br>";
+echo "法語";
+echo Carbon::parse('2019-07-23 14:51')->locale('fr_FR')->isoFormat('LLLL'); // 'mardi 23 juillet 2019 14:51'
+
+// ... but also does 'from now', 'after' and 'before'
+// rolling up to seconds, minutes, hours, days, months, years
+
+$daysSinceEpoch = Carbon::createFromTimestamp(0)->diffInDays();
 ?>
